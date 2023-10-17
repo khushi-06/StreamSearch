ae70007d1fc9fc543a44ff74831b3a1c074024d8
khushi gupta <guptakhushi622003@gmail.com>
2023-10-17 10:26:52 +0530

    added the header

---
+++ src/app/component/header/header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from '@/app/component/header/header.module.css'
+
+const header = () => {
+  return (
+    <>
+
+    < header className={styles.main_header}>
+        <div className={styles.navbar_brand}>
+
+            
+        </div>
+
+    </header>
+
+    </>
+  )
+}
+
+export default header
