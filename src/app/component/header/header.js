d0351f957dedc0299b899190ed774c3e58f16d59
khushi gupta <guptakhushi622003@gmail.com>
2023-10-18 14:44:33 +0530

    added header

--- src/app/component/header/header.js
+++ src/app/component/header/header.js
@@ -1,14 +1,14 @@
 import styles from "@/app/styles/navbar.module.css"
 import Link from "next/link";
 import Image from "next/image";
-import Nav from "@/app/components/Nav";
+import Nav from "@/app/component/Nav";
 
 const Header = () => {
     return (
         <header className={styles.main_header}>
             <div className={styles.navbar_brand}>
                 <Link href="/">
-                    <Image src="/logo.png" alt="my logo image" width={150} height={40}/>
+                    <Image src="/logo.png  " alt="my logo image" width={150} height={40}/>
                 </Link>
 
             </div>
--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,7 @@ import header from './component/header/header'
 const page = () => {
   return (
     <>
-    {/* hwllo  */}
-    <Headers/>
+   
     </>
   )
 }
